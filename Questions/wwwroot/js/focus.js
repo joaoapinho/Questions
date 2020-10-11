a0795db76b7de5c32341f8c1d38d250ebf5f846c
João Pinho <joaoapinho@gmail.com>
2020-10-12 00:39:57 +0100

    added connectivity feature

--- Questions/wwwroot/js/focus.js
+++ Questions/wwwroot/js/focus.js
@@ -1,3 +1,27 @@
 ﻿function focusInput(id) {
   document.getElementById(id).focus();
-} 
+}
+
+let handler;
+
+window.Connection = {
+  Initialize: function (interop) {
+
+    handler = function () {
+      interop.invokeMethodAsync("Connection.StatusChanged", navigator.onLine);
+    }
+
+    window.addEventListener("online", handler);
+    window.addEventListener("offline", handler);
+
+    handler(navigator.onLine);
+  },
+  Dispose: function () {
+
+    if (handler != null) {
+
+      window.removeEventListener("online", handler);
+      window.removeEventListener("offline", handler);
+    }
+  }
+};
