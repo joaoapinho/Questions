c5d4f25daa19ef37ead9ce881daf76ac453f6434
João Pinho <joaoapinho@gmail.com>
2020-10-11 22:30:39 +0100

    added details page and cached questions

--- Questions/Data/QuestionsService.cs
+++ Questions/Data/QuestionsService.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Questions.Data
 {
-  public class QuestionsService: IQuestionsService
+  public class QuestionsService : IQuestionsService
   {
     private readonly HttpClient httpClient;
 
@@ -20,6 +20,7 @@ namespace Questions.Data
         BaseAddress = new Uri(configuration.GetConnectionString("api"))
       };
     }
+
 
     public async Task<HttpResponseMessage> CheckHealth()
     {
@@ -38,10 +39,23 @@ namespace Questions.Data
       HttpResponseMessage response = await httpClient.GetAsync(QueryHelpers.AddQueryString("questions", queryStringParam));
       string content = await response.Content.ReadAsStringAsync();
       response.EnsureSuccessStatusCode();
+      var q = JsonSerializer.Deserialize<IEnumerable<Question>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+      return q;
+    }
 
-      var questions = JsonSerializer.Deserialize<IEnumerable<Question>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-      return questions;
+    public async Task<Question> GetQuestion(int id)
+    {
+      HttpResponseMessage response = await httpClient.GetAsync($"questions/{id}");
+      string content = await response.Content.ReadAsStringAsync();
+      response.EnsureSuccessStatusCode();
+      try
+      {
+        return JsonSerializer.Deserialize<Question>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+      }
+      catch
+      {
+        return null;
+      }
     }
 
 
--- Questions/Startup.cs
+++ Questions/Startup.cs
@@ -28,7 +28,8 @@ namespace Questions
       services.AddRazorPages();
       services.AddServerSideBlazor();
       services.AddHttpClient();
-            services.AddTransient<QuestionsService>();
+      services.AddTransient<QuestionsService>();
+      services.AddSingleton<QuestionsRepository>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
